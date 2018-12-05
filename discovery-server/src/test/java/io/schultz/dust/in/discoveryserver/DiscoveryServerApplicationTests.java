48b835e55dfde0cc70a40a4a87e927edf5a1bf63
niranjandevuni <niranjan.devuni@gmail.com>
2018-12-05 18:50:25 +0530

    Add files via upload

---
+++ discovery-server/src/test/java/io/schultz/dust/in/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,16 @@
+package io.schultz.dust.in.discoveryserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiscoveryServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
